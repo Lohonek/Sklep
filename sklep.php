6f3d11565918ce381b91621397467098e2b2f308
Lohonek <65301589+Lohonek@users.noreply.github.com>
2023-02-05 23:44:26 +0100

    Update sklep.php

--- sklep.php
+++ sklep.php
@@ -233,3 +233,49 @@ if($upust == "5"){
     </div>
 </body>
 </html>
+
+
+
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 05 Lut 2023, 23:44
+-- Wersja serwera: 10.4.25-MariaDB
+-- Wersja PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `mojsklep`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `dane`
+--
+
+CREATE TABLE `dane` (
+  `imie` text NOT NULL,
+  `nazwisko` text NOT NULL,
+  `kodpocztowy` text NOT NULL,
+  `ulica` text NOT NULL,
+  `miejscowosc` text NOT NULL,
+  `telefon` int(11) NOT NULL,
+  `cenakoncowa` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
